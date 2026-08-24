c5920bc2fc45a0c6fc97e984468bf50d285ec80b
liptan <liptankumar465@gmail.com>
2026-08-24 23:04:56 +0530

    Right Half Pyramid

---
+++ Pattern Printing Programs/1_Right_Half_Pyramid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+/*
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+*/
+int main()
+{
+    int row, col, num;
+    cout << "Enter the num: ";
+    cin >> num;
+
+    for (row = 1; row <= num; row++){
+        for (col = 1; col <= row; col++){
+            cout << "* ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
