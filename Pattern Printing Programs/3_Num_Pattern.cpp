ce05f995c91af71600c76edb5a269a2d31a04f5d
liptan <liptankumar465@gmail.com>
2026-08-25 08:45:18 +0530

    Update Numbers Pattern

--- Pattern Printing Programs/3_Num_Pattern.cpp
+++ Pattern Printing Programs/3_Num_Pattern.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 using namespace std;
 /*
+    1-> pattern
     1
     2 3               row => 1 to n
-    4 5 6             col => 1 to row (print count)
+    4 5 6             col => 1 to row (print count and ++)
     7 8 9 10
     11 12 13 14 15
+*/
+/*
+    2-> pattern
+    1
+    2 2                 row => 1 to n
+    3 3 3               col => 1 to row (print count) ++
+    4 4 4 4
+    5 5 5 5 5
+*/
+/*
+    3-> pattern
+    1
+    1 2                row => 1 to n
+    1 2 3              col => 1 to row (print col)
+    1 2 3 4
+    1 2 3 4 5
 */
 int main()
 {
@@ -13,6 +30,7 @@ int main()
     cout << "Enter the num: ";
     cin >> num;
 
+    cout << "\nPattern 1\n";
     int count = 1;
     for (row = 1; row <= num; row++)
     {
@@ -26,5 +44,33 @@ int main()
         cout << endl;
     }
 
+    cout << "\nPattern 2\n";
+    count = 1;
+    for (row = 1; row <= num; row++)
+    {
+        // col print
+        for (col = 1; col <= row; col++)
+        {
+            cout << count << " ";
+        }
+        count++;
+
+        cout << endl;
+    }
+
+    cout << "\nPattern 3\n";
+    count = 1;
+    for (row = 1; row <= num; row++)
+    {
+        // count print
+        for (col = 1; col <= row; col++)
+        {
+            cout << count << " ";
+            count++;
+        }
+        count = 1;
+
+        cout << endl;
+    }
     return 0;
 }
