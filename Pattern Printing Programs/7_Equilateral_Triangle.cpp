a99518410ddd7512ea4ac9d17d6da2f21438933f
liptan <liptankumar465@gmail.com>
2026-08-30 19:42:38 +0530

    Equilateral triangle

---
+++ Pattern Printing Programs/7_Equilateral_Triangle.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row, col, n;
+    printf("Enter the num:- ");
+    cin >> n;
+
+    for (row = 1; row <= n; row++)
+    {
+        // space print
+        for (col = 1; col <= n - row; col++)
+        {
+            cout << " ";
+        }
+        // * print
+        for (col = 1; col <= row; col++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
