a271a043e51471f1fe45b66dc23cb62a0b711052
liptan <liptankumar465@gmail.com>
2026-08-30 20:20:51 +0530

    Reverse floyd pattern

---
+++ Pattern Printing Programs/17_Reverse_Floyd.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printReverseFloyd(int n)
+{
+    int curr_val = n * (n + 1) / 2;
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = i; j >= 1; j--)
+        {
+            cout << setprecision(2);
+            cout << curr_val-- << "  ";
+        }
+
+        cout << endl;
+    }
+}
+
+// Driver's Code
+int main()
+{
+    int n = 7;
+    printReverseFloyd(n);
+    return 0;
+}
